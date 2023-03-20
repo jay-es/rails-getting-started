44f77c33e630ebd407f024d7722d60917a993fa4
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2023-03-20 13:50:30 +0000

    7.2 リソースフルルーティング

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index" # ArticlesController の indexアクションに対応
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  resources :articles
 end
