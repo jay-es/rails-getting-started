b5015ee568b782b85d957c001cd19bc036e0341b
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2023-03-20 11:59:00 +0000

    7.1 記事を1件表示する

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -6,4 +6,8 @@ class ArticlesController < ApplicationController
 
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "articles#index" # ArticlesController の indexアクションに対応
 
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
 end
