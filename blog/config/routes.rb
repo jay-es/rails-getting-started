85ca32dec65f93cf25bd833f835e9cace2489367
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2023-04-02 02:16:54 +0000

    8.3 コメントへのルーティングを追加する

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
-  resources :articles
+  # resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
